6e622a95d46f062afcdf9a1cd88fc5fb45e450e7
John Imrie <jgni@cpan.org>
2014-12-31 09:39:04 +0000

    Import of JGNI/Locale-CLDR-Locales-Br.pm-v0.26.7.tar.gz from CPAN.
    gitpan-cpan-distribution: Locale-CLDR-Locales-Br
    gitpan-cpan-version:      v0.26.7
    gitpan-cpan-path:         JGNI/Locale-CLDR-Locales-Br.pm-v0.26.7.tar.gz
    gitpan-cpan-author:       JGNI
    gitpan-cpan-maturity:     released

---
+++ lib/Locale/CLDR/Locales/Br/Any.pm
@@ -0,0 +1,19 @@
+package Locale::CLDR::Locales::Br::Any;
+
+# This file auto generated
+#	on Tue 30 Dec  9:13:24 pm GMT
+
+use version;
+
+our $VERSION = version->declare('v0.26.7');
+
+use v5.10;
+use mro 'c3';
+use if $^V ge v5.12.0, feature => 'unicode_strings';
+
+use Moose;
+
+extends('Locale::CLDR::Locales::Br');
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
---
+++ lib/Locale/CLDR/Locales/Br/Any/Fr.pm
@@ -0,0 +1,29 @@
+=head1
+
+Locale::CLDR::Locales::Br::Any::Fr - Package for language Breton
+
+=cut
+
+package Locale::CLDR::Locales::Br::Any::Fr;
+# This file auto generated from Data\common\main\br_FR.xml
+#	on Tue 30 Dec  9:13:24 pm GMT
+# XML file generated 2014-07-23 16:10:33 -0500 (Wed, 23 Jul 2014)
+
+use version;
+
+our $VERSION = version->declare('v0.26.7');
+
+use v5.10;
+use mro 'c3';
+use utf8;
+use if $^V ge v5.12.0, feature => 'unicode_strings';
+
+use Moose;
+
+extends('Locale::CLDR::Locales::Br::Any');
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+# vim: tabstop=4
